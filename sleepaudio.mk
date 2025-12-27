48232a3a4d08673dec9d9f25c724861f7a119e27
sleep-bugy <mohammadadisetiawan09@gmail.com>
2025-12-27 12:08:06 +0700

    refactor: Reorganize into Modular System Structure (App, Configs, SEPolicy)

--- sleepaudio.mk
+++ sleepaudio.mk
@@ -1,5 +1,16 @@
 # SleepAudio Integration Config
 # Usage: $(call inherit-product, packages/apps/SleepAudio/sleepaudio.mk)
 
+# 1. Main Package
 PRODUCT_PACKAGES += \
     SleepAudio
+
+# 2. Configurations
+PRODUCT_PACKAGES += \
+    privapp_whitelist_org.lineageos.sleepaudio
+
+# 3. SEPolicy (If BOARD_VENDOR_SEPOLICY_DIRS is handled by device, 
+# typically we don't add strictly here unless it's a board config, 
+# but we document it. For 'complex' look, we define variables)
+
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
